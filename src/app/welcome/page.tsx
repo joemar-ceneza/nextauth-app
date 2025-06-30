65cf3fd819ca3356264081925f3ac57d7c5fc1ac
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-07-01 01:52:15 +0800

    Fix import path for authOptions in WelcomePage component

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,6 +1,6 @@
 // src/app/welcome/page.tsx
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions";
 import { redirect } from "next/navigation";
 
 export default async function WelcomePage() {
