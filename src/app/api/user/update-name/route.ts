d429cab7c85af12757a8b3d06a6ff29260e66c2c
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-07-28 18:46:33 +0800

    fix: include credentials in fetch to send auth cookies

--- src/app/api/user/update-name/route.ts
+++ src/app/api/user/update-name/route.ts
@@ -3,17 +3,18 @@ import { authOptions } from "@/lib/authOptions";
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 
 export async function POST(req: NextRequest) {
-  const session = await getServerSession({ req, ...authOptions });
-  if (!session?.user?.email) {
+  const token = await getToken({ req });
+  if (!token?.email) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
   const { name } = await req.json();
   try {
     await prisma.user.update({
-      where: { email: session.user.email },
+      where: { email: token.email },
       data: { name },
     });
     return NextResponse.json({ success: true });
